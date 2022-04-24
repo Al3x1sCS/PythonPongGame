569673ad07e17b1d204f92f33323ded50a5d4e23
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-24 20:55:19 -0300

    FEAT: Iniciando o jogo PONG em Python. Por enquanto não sera utilizado PYGAME, apenas Turtle.

---
+++ PongGame.py
@@ -0,0 +1,11 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by AZ0iC")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game LOOP
+while True:
+    wn.update()
