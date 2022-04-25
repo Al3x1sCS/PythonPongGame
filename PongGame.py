a13890e2c9523e3b2d46d0b17ea378abe84cb417
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-24 22:58:10 -0300

    FEAT: Adicionando Controle. Foi adicionado teclas para movimentar os Paddles.

--- PongGame.py
+++ PongGame.py
@@ -7,7 +7,7 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
-paddle_a =turtle.Turtle()
+paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
@@ -16,7 +16,7 @@ paddle_a.penup()
 paddle_a.goto(-320, 0)
 
 # Paddle B
-paddle_b =turtle.Turtle()
+paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
@@ -25,13 +25,44 @@ paddle_b.penup()
 paddle_b.goto(320, 0)
 
 # Ball
-ball =turtle.Turtle()
+ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game LOOP
 while True:
