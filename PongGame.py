f281b1770dc29c8b0ca4054643cd31c537c86c17
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-24 23:10:44 -0300

    FIX: Alterando o codigo. Foi feito uma leve alteração no codigo, adicionado espaço entre as marcas e a marcação da Janela.

--- PongGame.py
+++ PongGame.py
@@ -1,12 +1,15 @@
 import turtle
 
+# Mark: Window
+
 wn = turtle.Screen()
 wn.title("Pong by AZ0iC")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Paddle A
+# Mark: Paddle A
+
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -15,7 +18,8 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-320, 0)
 
-# Paddle B
+# Mark: Paddle B
+
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -24,7 +28,8 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(320, 0)
 
-# Ball
+# Mark: Ball
+
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
@@ -33,7 +38,8 @@ ball.penup()
 ball.goto(0, 0)
 
 
-# Function
+# Mark: Function
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -44,6 +50,7 @@ def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
+
 
 def paddle_b_up():
     y = paddle_b.ycor()
@@ -57,13 +64,15 @@ def paddle_b_down():
     paddle_b.sety(y)
 
 
-# Keyboard binding
+# Mark: Keyboard binding
+
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-# Main game LOOP
+# Mark: Main game LOOP
+
 while True:
     wn.update()
