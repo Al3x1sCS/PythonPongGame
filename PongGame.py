4262f7065e307f7e9d73b41989bf063abbb21cda
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-24 22:21:35 -0300

    FEAT: Adicionando Paddle a e Paddle b. Foi adicionado dois paddles, um a direita e o outro a esquerda.

--- PongGame.py
+++ PongGame.py
@@ -6,6 +6,24 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+paddle_a =turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-320, 0)
+
+# Paddle B
+paddle_b =turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(320, 0)
+
 # Main game LOOP
 while True:
     wn.update()
