61892662b5291629e110930df13a2bb8adc73728
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-25 11:49:11 -0300

    FEAT: Adicionando movimento a bolla. Foi adicionado movimento a bolla, mas a velocidade da bola depende do computador.

--- PongGame.py
+++ PongGame.py
@@ -36,6 +36,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = 0.1
 
 
 # Mark: Function
@@ -76,3 +78,12 @@ wn.onkeypress(paddle_b_down, "Down")
 
 while True:
     wn.update()
+
+    # MARK:  Move the Ball
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Mark: Border cheking
+
+
