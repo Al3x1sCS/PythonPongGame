72faad8b4f7f6eed59e90ac7cbce444a3867c908
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-25 15:12:35 -0300

    FEAT: Adicionando Border checking e collisions. Foi adicionada condições de colisão na borda e nos paddles.

--- PongGame.py
+++ PongGame.py
@@ -36,8 +36,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.1
-ball.dy = 0.1
+ball.dx = 0.13
+ball.dy = -0.13
 
 
 # Mark: Function
@@ -85,5 +85,26 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Mark: Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
 
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
 
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Mark: Paddles and Ball collisions
+
+    if (ball.xcor() > 300 and ball.xcor() < 340) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(300)
+        ball.dx *= -1
+    if (ball.xcor() < -300 and ball.xcor() > -340) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-300)
+        ball.dx *= -1
