785cd6c7318c87cf45ba160532343ce740c99d57
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-04-24 22:27:39 -0300

    FEAT: Adicionando Ball. Foi adicionado uma bola no centro da tela.

--- PongGame.py
+++ PongGame.py
@@ -24,6 +24,15 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(320, 0)
 
+# Ball
+ball =turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
 # Main game LOOP
 while True:
     wn.update()
